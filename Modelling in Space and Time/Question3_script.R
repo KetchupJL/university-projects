a5991291110d5030bc363be5e6268db7d1306b77
James <james066lewis@gmail.com>
2025-04-18 20:28:52 +0100

    updating

--- Modelling in Space and Time/Question3_script.R
+++ Modelling in Space and Time/Question3_script.R
@@ -98,3 +98,72 @@ summary_table %>%
     Max_Temp = "Max (°C)",
     SD_Temp = "SD (°C)"
   )
+
+
+##############################################################################
+##############################################################################
+##############################################################################
+
+
+# Part B
+
+
+temps_13 <- temps_long %>%
+  filter(Date == as.Date("2012-12-13")) %>%
+  select(Site, Temp) %>%
+  left_join(meta %>% select(Location, Long, Lat), by = c("Site" = "Location")) %>%
+  mutate(
+    Long = case_when(
+      Site == "San.Francisco" ~ -122.4269,
+      Site == "San.Diego" ~ -117.1831,
+      Site == "Santa.Cruz" ~ -121.9911,
+      Site == "Death.Valley" ~ -116.8669,
+      TRUE ~ Long
+    ),
+    Lat = case_when(
+      Site == "San.Francisco" ~ 37.7705,
+      Site == "San.Diego" ~ 32.7336,
+      Site == "Santa.Cruz" ~ 36.9905,
+      Site == "Death.Valley" ~ 36.4622,
+      TRUE ~ Lat
+    )
+  )
+
+# Empirical variogram
+
+# Prediction Sites
+pred_sites <- c("San.Diego", "Fresno")
+
+# Training Data (sites used to fit the model)
+train_data <- temps_13 %>%
+  filter(!Site %in% pred_sites)
+
+# Test Data (sites we will predict)
+test_data <- temps_13 %>%
+  filter(Site %in% pred_sites)
+
+library(geoR)
+
+geo_data <- as.geodata(train_data, coords.col = c("Long", "Lat"), data.col = "Temp")
+
+vario_emp <- variog(geo_data, max.dist = 600, option='bin')
+
+plot(vario_emp,
+     main = "Empirical Variogram of Max Temp (13 Dec 2012)",
+     xlab = "Distance (km)",
+     ylab = "Semivariance",
+     pch = 19, cex = 1.2, col = "black")
+lines(lowess(vario_emp$u, vario_emp$v), col = "blue", lwd = 2)
+
+
+######
+# fit Gaussian Process (GP) models via Maximum Likelihood Estimation
+
+
+
+xv_mle <- xvalid(geo_data, model = model_mle)
+
+
+# Check the summary of the Exponential model
+summary(mle_model_exp)
+
